734d18f9b4fc36764d1072e64cc102bb98dfab08
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2021-01-03 19:45:09 -0800

    more binary search tree note

--- algorithms/ex02_valid_subsequence.js
+++ algorithms/ex02_valid_subsequence.js
@@ -1,15 +1,69 @@
-function validSubsequence(array, sequence){
+// subsequence is a important concept
+// need to match number and the order
+// can use 2 pointers method
+
+
+// O(N) O(1)
+function validSubsequence4(array, sequence){ // other key 2
+    let seqIdx = 0;
+    for(let i = 0; i < array.length; i++){
+        if(seqIdx === sequence.length) break;
+        if(array[i] === sequence[seqIdx]) seqIdx++;
+    }
+    return seqIdx === sequence.length;
+}
+
+function validSubsequence3(array, sequence){ // other key
+    let arrIdx = 0;
+    let seqIdx = 0;
+    while(arrIdx < array.length && seqIdx < sequence.length){
+        if(array[arrIdx] === sequence[seqIdx]){
+            seqIdx++;
+        }
+        arrIdx++;
+    }
+    if(seqIdx === sequence.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validSubsequence2(array, sequence){ // work
+    let arrIdx = 0;
+    let seqIdx = 0;
+    let count = 0;
+    while(arrIdx < array.length && seqIdx < sequence.length){
+        if(array[arrIdx] === sequence[seqIdx]){
+            seqIdx++;
+            arrIdx++;
+            count++;
+        } else {
+            arrIdx++;
+        }
+    }
+    if(count === sequence.length){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function validSubsequence1(array, sequence){ // fail
     const totalMatch = sequence.length;
     let count = 0;
     const storedSequence = {};
-    for(let i = 0; i < totalMatch; i++){
-        storedSequence[sequence[i]] = true;
+    for(let i = 0; i < array.length; i++){
+        storedSequence[array[i]] = true;
     }
-    for(let j = 0; j < array.length; j++){
-        if(storedSequence[array[j]]){
+    while(count < totalMatch){
+        if(storedSequence[sequence[count]]){
+            console.log("count=" + count);
             count++;
+        } else {
+            return false;
         }
-        console.log("count=" + count);
     }
     if(count >= totalMatch){
         return true;
@@ -17,3 +71,5 @@ function validSubsequence(array, sequence){
         return false;
     }
 }
+
+console.log(validSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]));
