001735ca50ffaf38c84ca2305307f2e17e58899a
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2021-01-22 02:51:45 -0800

    notes about DOM

---
+++ Udemy/S7_JS_DOM_Events/DOM.js
@@ -0,0 +1,14 @@
+// What is the DOM?
+
+// Document object model
+// Structured representation of HTML documents
+// JavaScript manipulate HTML elements and styles 
+// So you can change HTML and the styles
+// DOM is a type of API (WEB APIs)
+
+// DOM is basically a connection point of HTML element and JS
+
+// DOM is generated when the HTML loaded by a browser
+// It's a tree structure
+
+// DOM is not JS
